199a5352bae91bebef3094ddd916730b19082db4
Adam Freemer <adam@freemer.com>
2021-02-07 23:54:43 -0500

    update database to postgres for heroku deploy

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2021_02_07_202310) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "locations", force: :cascade do |t|
     t.string "store_number"
     t.boolean "slot_1"
