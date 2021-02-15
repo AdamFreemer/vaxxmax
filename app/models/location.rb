3011ce535167f651577f0418e672e778c05a94be
Adam Freemer <adam@freemer.com>
2021-02-15 12:27:36 -0500

    Add donation and clickable address

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,2 +1,5 @@
 class Location < ApplicationRecord
+  def google_search
+    "https://www.google.com/maps/search/?api=1&query=#{CGI.escape full_address.force_encoding('ASCII-8BIT')}+Rite+Aid"
+  end
 end
