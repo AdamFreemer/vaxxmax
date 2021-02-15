e9d3a61e24d3e38a938d66c1db91159fcb820b29
Adam Freemer <adam@freemer.com>
2021-02-15 14:25:32 -0500

    Add store website link

--- app/models/location.rb
+++ app/models/location.rb
@@ -2,4 +2,12 @@ class Location < ApplicationRecord
   def google_search
     "https://www.google.com/maps/search/?api=1&query=#{CGI.escape full_address.force_encoding('ASCII-8BIT')}+Rite+Aid"
   end
+
+  def rite_aid_location_website
+    st = state&.parameterize(separator: '-')
+    add = address&.parameterize(separator: '-')
+    cit = city&.parameterize(separator: '-')
+
+    "https://www.riteaid.com/locations/#{st}/#{cit}/#{add}.html"
+  end
 end
