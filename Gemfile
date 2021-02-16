767f4e78a2bbb8262d7368cf380df993692a56b3
Adam Freemer <adam@freemer.com>
2021-02-16 18:15:46 -0500

    Moved google code to index page

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 ruby '2.7.2'
 
 gem 'bootsnap', '>= 1.4.4', require: false
+gem 'google-analytics-rails', '1.1.1'
 gem 'jbuilder', '~> 2.7'
 gem 'jquery-rails'
 gem 'net-http-persistent'
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,7 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
+  GA.tracker = "UA-112233-4"
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
