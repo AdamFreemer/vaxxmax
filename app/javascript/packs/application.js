2a8027c2208fc788269581186fdabd35ec780f96
Adam Freemer <adam@freemer.com>
2021-02-25 00:48:37 -0500

    Scroller stubbed in

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,7 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+require("packs/jquery.jConveyorTicker.min")
 
 Rails.start()
 Turbolinks.start()
