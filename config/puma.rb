874ad772714001eaef2eb7d225dda03d3b408d83
Adam Freemer <adam@freemer.com>
2021-02-26 12:20:25 -0500

    Update concurrenct

--- config/puma.rb
+++ config/puma.rb
@@ -1,6 +1,6 @@
 # As per https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server
 
-workers Integer(ENV['WEB_CONCURRENCY'] || 3)
+workers Integer(ENV['WEB_CONCURRENCY'] || 5)
 threads_count = Integer(ENV['RAILS_MAX_THREADS'] || 5)
 threads threads_count, threads_count
 
