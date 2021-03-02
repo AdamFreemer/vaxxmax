c5ff76eb686847319409c74383746fd52b9f00b4
Adam Freemer <adam@freemer.com>
2021-03-02 15:12:26 -0500

    Thread count to 4

--- config/puma.rb
+++ config/puma.rb
@@ -1,7 +1,7 @@
 # As per https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server
 
 workers Integer(ENV['WEB_CONCURRENCY'] || 6)
-threads_count = Integer(ENV['RAILS_MAX_THREADS'] || 6)
+threads_count = Integer(ENV['RAILS_MAX_THREADS'] || 4)
 threads threads_count, threads_count
 
 preload_app!
