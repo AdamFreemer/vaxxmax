1dcf6d1e1b84e6cc26dfe7a352a05ec78a27402d
Adam Freemer <adam@freemer.com>
2021-03-02 17:09:41 -0500

    dd

--- config/puma.rb
+++ config/puma.rb
@@ -1,15 +1,14 @@
 # As per https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server
 
 workers Integer(ENV['WEB_CONCURRENCY'] || 6)
-threads_count = Integer(ENV['RAILS_MAX_THREADS'] || 8)
+threads_count = Integer(ENV['RAILS_MAX_THREADS'] || 5)
 threads threads_count, threads_count
 
 preload_app!
 
 rackup            DefaultRackup
 port              ENV['PORT']                       || 3000
-reaping_frequency ENV['DATABASE_REAPING_FREQUENCY'] || 10
-environment       ENV['RACK_ENV']                         || 'development'
+environment       ENV['RACK_ENV']                   || 'development'
 
 before_fork do
   # Start Heroku's Ruby runtime metrics collection
