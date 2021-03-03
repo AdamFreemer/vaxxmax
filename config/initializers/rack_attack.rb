5097c6a9126cb8e77c69e46331c7375a5585bd09
Adam Freemer <adam@freemer.com>
2021-03-02 20:52:25 -0500

    modify setup

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -1,11 +1,13 @@
-# Lockout IP addresses that are hammering your login page.
-# After 20 requests in 1 minute, block all requests from that IP for 1 hour.
-Rack::Attack.blocklist('allow2ban login scrapers') do |req|
-  Rack::Attack::Allow2Ban.filter(req.ip, maxretry: 20, findtime: 1.minute, bantime: 1.hour) do
-    # The count for the IP is incremented if the return value is truthy.
-    req.path == '/' and req.get?
-    req.path == '/walgreens' and req.get?
-    req.path == '/riteaid' and req.get?
+# rack_attack
+class Rack::Attack
+  Rack::Attack.cache.store = ActiveSupport::Cache::MemoryStore.new
+
+  Rack::Attack.blocklist('allow2ban login scrapers') do |req|
+    Rack::Attack::Allow2Ban.filter(req.ip, maxretry: 20, findtime: 1.minute, bantime: 1.hour) do
+      # The count for the IP is incremented if the return value is truthy.
+      req.path == '/' and req.get?
+      req.path == '/walgreens' and req.get?
+      req.path == '/riteaid' and req.get?
+    end
   end
 end
-
