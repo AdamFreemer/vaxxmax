3cf275f4b91263a67978b1b82a9ae49a6694e280
Adam Freemer <adam@freemer.com>
2021-03-02 19:22:45 -0500

    Add rack-attack

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -8,6 +8,9 @@ Rails.application.configure do
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
 
+  # Rack Attack replace rack
+  config.middleware.use Rack::Attack
+
   # Do not eager load code on boot.
   config.eager_load = false
 
---
+++ config/initializers/rack_attack.rb
@@ -0,0 +1,12 @@
+# Lockout IP addresses that are hammering your login page.
+# After 20 requests in 1 minute, block all requests from that IP for 1 hour.
+Rack::Attack.blocklist('allow2ban login scrapers') do |req|
+  Rack::Attack::Allow2Ban.filter(req.ip, maxretry: 20, findtime: 1.minute, bantime: 1.hour) do
+    # The count for the IP is incremented if the return value is truthy.
+    req.path == '/' and req.get?
+    req.path == '/walgreens' and req.get?
+    req.path == '/rite_aid' and req.get?
+  end
+end
+
+Rack::Attack.cache.store = ActiveSupport::Cache::MemoryStore.new
