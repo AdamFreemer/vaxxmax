99925a12ee412f67d2b543f35cc6cc93e168878a
Adam Freemer <adam@freemer.com>
2021-03-19 15:34:39 -0700

    sidekiq database config

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -8,4 +8,17 @@ if Rails.env != "development"
   Sidekiq.configure_server do |config|
     config.redis = {url: ENV["HEROKU_REDIS_BLUE_URL"] }
   end
+
+  Rails.application.config.after_initialize do
+    ActiveRecord::Base.connection_pool.disconnect!
+
+    ActiveSupport.on_load(:active_record) do
+      config = Rails.application.config.database_configuration[Rails.env]
+      config['reaping_frequency'] = ENV['DATABASE_REAP_FREQ'] || 10 # seconds
+      config['pool'] = ENV["SIDEKIQ_DATABASE_POOL_SIZE"] || 1
+      ActiveRecord::Base.establish_connection(config)
+    end
+
+  end
+
 end
