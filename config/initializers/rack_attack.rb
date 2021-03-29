a368927ae8eebe54426153beff978bd235110d3c
Adam Freemer <adam@freemer.com>
2021-03-29 10:02:13 -0400

    Disengage rack attack

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 class Rack::Attack
-  throttle('requests by ip', limit: 100, period: 900) do |request|
-    request.ip
-  end
+  # throttle('requests by ip', limit: 100, period: 900) do |request|
+  #   request.ip
+  # end
 
   def remote_ip
     @remote_ip ||= (env['action_dispatch.remote_ip'] || ip).to_s
