38ae544ce3baaaf5110821701437d1d9bf21d2d2
Adam Freemer <adam@freemer.com>
2021-05-04 09:53:56 -0400

    Remove routes in attempt to block offending ip

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,17 @@
 require 'sidekiq/web'
 
 Rails.application.routes.draw do
-  root to: 'locations#riteaid'
+  root to: 'locations#cvs'
 
   # pages
   get 'cvs', to: 'locations#cvs'
-  get 'health_mart', to: 'locations#health_mart'
-  get 'riteaid', to: 'locations#riteaid'
-  get 'rite_aid', to: 'locations#riteaid'
-  get 'walgreens', to: 'locations#walgreens'
-  get 'walmart', to: 'locations#walmart'
-  get 'all', to: 'locations#all'
-  get 'map', to: 'visualizations#main'
+  # get 'health_mart', to: 'locations#health_mart'
+  # get 'riteaid', to: 'locations#riteaid'
+  # get 'rite_aid', to: 'locations#riteaid'
+  # get 'walgreens', to: 'locations#walgreens'
+  # get 'walmart', to: 'locations#walmart'
+  # get 'all', to: 'locations#all'
+  # get 'map', to: 'visualizations#main'
 
   # zipcode geolocate set
   get 'set_zipcode/:zipcode', to: 'locations#set_zipcode'
@@ -20,10 +20,10 @@ Rails.application.routes.draw do
   post 'cvs_ingest', to: 'data_collections#cvs_ingest'
 
   # api
-  get 'api/v1/state_by_zipcode/:state/:provider', to: 'data_collections#state_by_zipcode'
-  get 'api/v1/daily_by_state/:state/:provider/:day_of_year', to: 'data_collections#daily_by_state'
-  get 'api/v1/daily_by_state_count/:state/:provider/:day_of_year', to: 'data_collections#daily_by_state_count'
-  get 'api/v1/by_zipcode/:state/:provider/:start_day/:end_day', to: 'data_collections#by_zipcode'
+  # get 'api/v1/state_by_zipcode/:state/:provider', to: 'data_collections#state_by_zipcode'
+  # get 'api/v1/daily_by_state/:state/:provider/:day_of_year', to: 'data_collections#daily_by_state'
+  # get 'api/v1/daily_by_state_count/:state/:provider/:day_of_year', to: 'data_collections#daily_by_state_count'
+  # get 'api/v1/by_zipcode/:state/:provider/:start_day/:end_day', to: 'data_collections#by_zipcode'
 
   # per page select dropdowns
   get 'set_state_cvs/:state_cvs', to: 'locations#set_state_cvs'
